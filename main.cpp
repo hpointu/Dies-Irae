a10920c59f79faa38b38b7f6980e112f7adbc4f5
freddy <tedefump@gmail.com>
2010-12-06 09:27:42 +0100

    Test sfml

--- main.cpp
+++ main.cpp
@@ -19,9 +19,13 @@ int main(int argc, char *argv[])
 	sf::RenderWindow window ;
 	window.Create(sf::VideoMode(250, 170, 32), "Dies Irae", sf::Style::Close);
 
+	sf::Shape sh = sf::Shape::Circle(0.f,0.f,100.f, sf::Color(0,0,0,0), 2.f, sf::Color(255,255,255));
+	sh.SetPosition(100.f, 100.f);
 	while(window.IsOpened())
 	{
+		window.Clear();
 		eventManager(window);
+		window.Draw(sh);
 		window.Display();
 	}
 
